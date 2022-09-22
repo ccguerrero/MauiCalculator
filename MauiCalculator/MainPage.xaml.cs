2dfa15a111d7c6f99ef36b36da7cd405cef20110
Cuauhtemoc Guerrero Hernandez <C0497660@intra.camosun.bc.ca>
2022-09-22 13:15:15 -0700

    Agregar archivos de proyecto.

---
+++ MauiCalculator/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace MauiCalculator;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void entryNum1_TextChanged(object sender, TextChangedEventArgs e)
+	{
+
+	}
+
+	private void entryNum2_TextChanged(object sender, TextChangedEventArgs e)
+	{
+
+	}
+
+	private void btnAdd_Clicked(object sender, EventArgs e)
+	{
+
+	}
+
+	private void btnSubstract_Clicked(object sender, EventArgs e)
+	{
+
+	}
+
+	private void btnMultiply_Clicked(object sender, EventArgs e)
+	{
+
+	}
+
+	private void btnDivide_Clicked(object sender, EventArgs e)
+	{
+
+	}
+}
+
