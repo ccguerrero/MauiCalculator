530dd0aa399c3436bf4c782033d7dbb10ff11aa3
Cuauhtemoc Guerrero Hernandez <C0497660@intra.camosun.bc.ca>
2022-09-29 14:34:37 -0700

    Added comments

--- MauiCalculator/MainPage.xaml.cs
+++ MauiCalculator/MainPage.xaml.cs
@@ -36,15 +36,18 @@ public partial class MainPage : ContentPage
 	public string correctNum1="";
 	public string correctNum2="";
 
+
 	private void entryNum1_TextChanged(object sender, TextChangedEventArgs e)
 	{
-		double NumTemp = 0;
+        // Validate entry number 1 is a valid double number
+        double NumTemp = 0;
 		if(double.TryParse(entryNum1.Text, out NumTemp))
 		{
 			correctNum1 = entryNum1.Text;  // correct number!!!
 		}
 		else
 		{
+			//Highlight the position where a non valid character is input
 			entryNum1.CursorPosition=correctNum1.Length;
 			int selectionLength = e.NewTextValue.Length - correctNum1.Length;
 			if (selectionLength > 0)
@@ -56,6 +59,7 @@ public partial class MainPage : ContentPage
 
 	private void entryNum2_TextChanged(object sender, TextChangedEventArgs e)
 	{
+        // Validate entry number 2 is a valid double number
         double NumTemp = 0;
 		if (double.TryParse(entryNum2.Text, out NumTemp))
 		{
@@ -63,6 +67,7 @@ public partial class MainPage : ContentPage
 		}
 		else
         {
+            //Highlight the position where a non valid character is input
             entryNum2.CursorPosition = correctNum2.Length;
             int selectionLength = e.NewTextValue.Length - correctNum2.Length;
             if (selectionLength >= 0)
@@ -71,8 +76,7 @@ public partial class MainPage : ContentPage
             }
         }
     }
-
-	private void btnAdd_Clicked(object sender, EventArgs e)
+	private void btnAdd_Clicked(object sender, EventArgs e)			//Addition button calculations with input validation
 	{
 		double Num1 = InputValidate.GetInput(entryNum1.Text);
 		double Num2 = InputValidate.GetInput(entryNum2.Text);
@@ -80,7 +84,7 @@ public partial class MainPage : ContentPage
 		txtResult.Text = Convert.ToString(Result);
 	}
 
-	private void btnSubstract_Clicked(object sender, EventArgs e)
+	private void btnSubstract_Clicked(object sender, EventArgs e)	//Substraction button calculations with input validation
 	{
         double Num1 = InputValidate.GetInput(entryNum1.Text);
         double Num2 = InputValidate.GetInput(entryNum2.Text);
@@ -88,23 +92,24 @@ public partial class MainPage : ContentPage
         txtResult.Text = Convert.ToString(Result);
     }
 
-	private void btnMultiply_Clicked(object sender, EventArgs e)
-	{
+	private void btnMultiply_Clicked(object sender, EventArgs e)    //Multiply button calculations with input validation
+    {
         double Num1 = InputValidate.GetInput(entryNum1.Text);
         double Num2 = InputValidate.GetInput(entryNum2.Text);
         double Result = Num1 * Num2;
         txtResult.Text = Convert.ToString(Result);
     }
 
-	private void btnDivide_Clicked(object sender, EventArgs e)
-	{
+	private void btnDivide_Clicked(object sender, EventArgs e)      //Divide button calculations with input validation
+    {
         double Num1 = InputValidate.GetInput(entryNum1.Text);
         double Num2 = InputValidate.GetInput(entryNum2.Text);
         double Result = Num1 / Num2;
         txtResult.Text = Convert.ToString(Result);
     }
-    private void btnSqr_Clicked(object sender, EventArgs e)
+    private void btnSqr_Clicked(object sender, EventArgs e)			//Square button calculations with input validation
     {
+		// Clicked count to display name after 5 times
 		count++;
 		if (count == 5)
 		{
@@ -114,7 +119,7 @@ public partial class MainPage : ContentPage
 		else
 			myName.Text = "";
 			
-	//	SemanticScreenReader.Announce(myName.Text);
+	//	SemanticScreenReader.Announce(myName.Text);					//ToDo -- check SematicScreenReader use
 			
         double Num1 = InputValidate.GetInput(entryNum1.Text);
         double Result = Num1 * Num1;
